541b99a8b49c3176b6f6128a915b8c83339a165a
Leisa Kalaniuvalu <l.kalaniuvalu@gmail.com>
2023-02-12 11:29:26 +1300

    first draft of portfolio made via react to showcase work

---
+++ src/Components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section id="about">
+            <h2>About Me</h2>
+            <p>I'm a web developer with a passion for building beautiful and functional websites. My experience includes working with HTML, CSS, JavaScript, and a variety of front-end frameworks.</p>
+        </section>
+    );
+};
+
+export default About;
---
+++ src/Components/Projects.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <section id="projects">
+            <h2>My Projects</h2>
+            <ul>
+                <li>
+                    <h3>Project 1</h3>
+                    <p>A brief description of project 1</p>
+                </li>
+                <li>
+                    <h3>Project 2</h3>
+                    <p>A brief description of project 2</p>
+                </li>
+            </ul>
+        </section>
+    );
+};
+
+export default Projects;
