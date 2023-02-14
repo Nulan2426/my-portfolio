81dd42a6fe960e49c16cad22bfaee141193c13b9
Leisa Kalaniuvalu <l.kalaniuvalu@gmail.com>
2023-02-14 13:27:29 +1300

    udpated contact form and have finally got Emailjs to work! updated header with working links to socials!

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,36 +1,74 @@
 import React from "react";
 import { useState } from "react";
+import emailjs from 'emailjs-com';
 
 
 const Contact = () => {
+
+    const [formData, setFormData] = useState({
+        user_name: "",
+        user_email: "",
+        message: ""
+    });
+
+    const handleChange = e => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });;
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        emailjs.send('service_weac45x',
+            'template_pyx7bck',
+            formData,
+            'd2ofAUCX57jxIkz7e')
+            .then((response) => {
+                console.log('Email successfully sent!');
+                alert("Leisa here, your message has been sent. I appreciate you reaching out!");
+                setFormData({
+                    user_name: "",
+                    user_email: "",
+                    message: ""
+                });
+            }, (err) => {
+                console.error('Failed to send email: ', err);
+            });
+    };
+
     
     return (
         <section id="contact">
             <h2>Contact Me</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                     type="text"
                     name="user_name"
                     placeholder="Your name"
+                    value={formData.user_name}
+                    onChange={handleChange}
                 
                 />
                 <input
                     type="email"
                     name="user_email"
                     placeholder="Your email"
+                    value={formData.user_email}
+                    onChange={handleChange}
                  
                 />
                 <textarea
                     name="message"
                     placeholder="Your message"
-                
+                    value={formData.message}
+                    onChange={handleChange}
                 />
                 <button type="submit">Submit</button>
-
             </form>
         </section>
     )
 }
 
-export default Contact
+export default Contact;
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import GitHub from '../Assets/github3.png';
+import LinkedIn from '../Assets/linkedin.png';
+import InstagramIcon from '../Assets/instagram.png';
+import "./Header.css"
+
 
 const Header = () => {
+
+  const SocialLink = ({ icon, url }) => (
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <img src={icon} alt="Social media link" />
+    </a>
+  )
+
   return (
     <header>
       <h1>Leisa Kalaniuvalu</h1>
+      <div className="header-social-links">
+        <SocialLink icon={GitHub} url="https://github.com/Nulan2426" />
+        <SocialLink icon={LinkedIn} url="https://www.linkedin.com/in/leisa-kalaniuvalu/" />
+        <SocialLink icon={InstagramIcon} url="https://www.instagram.com/lanzn/" />
+      </div>
       <nav>
         <ul>
           <li><a href="#about">About</a></li>
